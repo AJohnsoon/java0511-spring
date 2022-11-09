e25760e30744726bf0e041c1bc0cddf9c97c644e
ajohnsoon <allanjohnson.leal@gmail.com>
2022-11-09 19:05:42 -0300

    feat: add post resources mapping

---
+++ src/main/java/com/example/jvspace/dto/PostDTO.java
@@ -0,0 +1,59 @@
+package com.example.jvspace.dto;
+
+import com.example.jvspace.entities.Post;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+public class PostDTO implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+
+    public PostDTO(){}
+
+    public PostDTO(Post postEntity){
+        id = postEntity.getId();
+        date = postEntity.getDate();
+        title = postEntity.getTitle();
+        body = postEntity.getBody();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
--- src/main/java/com/example/jvspace/services/PostService.java
+++ src/main/java/com/example/jvspace/services/PostService.java
@@ -1,2 +1,55 @@
-package com.example.jvspace.services;public class PostService {
+package com.example.jvspace.services;
+
+import com.example.jvspace.entities.Post;
+import com.example.jvspace.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public List<Post> findAll(){
+        return postRepository.findAll();
+    }
+
+    public Post findById(String id){
+        Optional<Post> postObject = postRepository.findById(id);
+        return postObject.orElseThrow(() -> new RuntimeException());
+    }
+
+    public Post insert(Post postObject){
+        return postRepository.insert(postObject);
+    }
+
+    public Post update(String id, Post post){
+        try{
+            Post updateEntity = findById(id);
+            updateData(updateEntity, post);
+            return postRepository.save(updateEntity);
+        }
+        catch (RuntimeException e){
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void delete(String id){
+        try{
+            postRepository.deleteById(id);
+        }
+        catch (IllegalArgumentException e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void updateData(Post entity, Post post) {
+        entity.setTitle(post.getTitle());
+        entity.setBody(post.getBody());
+    }
 }
